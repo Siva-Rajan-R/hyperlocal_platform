4733ddb00a9fb6fba2732183404879b8f3824d31
Siva-Rajan-R <siva967763@gmail.com>
2025-12-30 14:14:07 +0530

    updated basemodel

--- hyperlocal_platform/core/basemodels/readdb_model.py
+++ hyperlocal_platform/core/basemodels/readdb_model.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional,Any
 
 class ReadDbBaseModel(BaseModel):
-    payload:dict
+    payload:Any
     method:str
     condition:Optional[dict]=None
