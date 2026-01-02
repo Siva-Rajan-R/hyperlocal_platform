da88d74082e945bb8d9d8f97a8090f0949f17c0c
Siva-Rajan-R <siva967763@gmail.com>
2026-01-02 13:53:22 +0530

    updated routing key builder

--- hyperlocal_platform/core/utils/routingkey_builder.py
+++ hyperlocal_platform/core/utils/routingkey_builder.py
@@ -1,4 +1,8 @@
 from ..enums.routingkey_enum import RoutingkeyActions,RoutingkeyState,RoutingkeyVersions
 
-def generate_routingkey(domain:str,action:RoutingkeyActions,state:RoutingkeyState,version:RoutingkeyVersions):
-    return f"{domain}.{action.value}.{state.value}.{version.value}"
+def generate_routingkey(domain:str,work_for:str,action:RoutingkeyActions,state:RoutingkeyState,version:RoutingkeyVersions):
+    """
+    The domain is the current service name, work-for is a service name of who triggered,
+
+    """
+    return f"{domain.lower()}.{work_for.lower()}.{action.value.lower()}.{state.value.lower()}.{version.value.lower()}"
